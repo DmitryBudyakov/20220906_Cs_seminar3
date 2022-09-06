e0b6ed53ca4a4495e245952e2f73f863511cb012
Dmitry Budyakov <dnikbu@yandex.ru>
2022-09-06 22:13:18 +0300

    Task22 с форматом вывода

---
+++ Task22/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 22: Напишите программу, которая
+// принимает на вход число (N) и выдаёт таблицу
+// квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void Square(int num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        int square = count * count;
+        Console.WriteLine($"{count} -> {square,3}");    // 3 - кол-во символов выводим во вторых скобках
+        count++;
+    }
+}
+
+Square(number);
